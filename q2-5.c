4e096c936e071494a2d491e959da481cfa289cae
Ketrean Prates <ketreanprates@gmail.com>
2026-08-17 20:31:58 -0300

    Add files via upload

---
+++ q2-5.c
@@ -0,0 +1,57 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int mes;
+    
+    printf("Informe o número do mês:\n");
+    scanf("%d", & mes);
+    
+    switch(mes) {
+        case 1:
+            printf("%d | Janeiro", mes);
+            break;
+        
+        case 2:
+            printf("%d | Fevereiro", mes);
+            break;
+        
+        case 3:
+            printf("%d | Março", mes);
+            break;
+            
+        case 4:
+            printf("%d | Abril", mes);
+            break;
+            
+        case 5:
+            printf("%d | Maio", mes);
+            break;
+        
+        case 6:
+            printf("%d | Junho", mes);
+            break;
+        
+        case 7:
+            printf("%d | Julho", mes);
+            break;
+            
+        case 8:
+            printf("%d | Agosto", mes);
+            break;
+        case 9:
+            printf("%d | Janeiro", mes);
+            break;
+    }
+    
+    return 0;
+}
